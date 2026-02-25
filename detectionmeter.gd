2b21705058730212f563af40e6415b81b6349575
Andrew <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2026-02-25 05:09:14 -0800

    Added a decay delay and some cool symbols to detection ring

--- detectionmeter.gd
+++ detectionmeter.gd
@@ -1,29 +1,41 @@
 extends Control
+signal detected
 
 @export_group("detection settings")
 @export var maxSeenTime:float;
 @export var decayRate:float;
+@export var decayDelay:float = 0.2;
 
-@export_group("vignette")
+@export_group("visuals")
 @export var vignetteMaxAlpha:float = 0.5;
 @export var vignetteStartPercent:float = 0.5;
 
+@export var ringMaxAlpha = 0.447058824;
 
 @export var canPhilSeePlayer:bool = false;
 var currentSeenTime:float;
-var vignetteColor:Color;
+var timeAfterStoppedBeingSeen:float;
 
 func setCanPhilSeePlayer(canSeePlayer:bool):
 	self.canPhilSeePlayer = canPhilSeePlayer
+	
+func setSettings(maxSeenTime:float, decayRate:float, decayDelay:float):
+	self.maxSeenTime = maxSeenTime;
+	self.decayRate = decayRate;
+	self.decayDelay = decayDelay;
 
 func _ready() -> void:
-	vignetteColor = $vignette.modulate;
+	pass
 
 func _process(delta: float) -> void:
 	if canPhilSeePlayer:
 		currentSeenTime += delta;
+		timeAfterStoppedBeingSeen = 0;
 	else:
-		currentSeenTime -= delta * decayRate
+		timeAfterStoppedBeingSeen += delta;
+		if(timeAfterStoppedBeingSeen > decayDelay):
+			currentSeenTime -= delta * decayRate;
+		
 	
 	# prevent seen time from going negative
 	currentSeenTime = clampf(currentSeenTime, 0.0, maxSeenTime);
@@ -31,7 +43,12 @@ func _process(delta: float) -> void:
 	var detectionPercentage = clampf(currentSeenTime / maxSeenTime, 0.0, 1.0);
 	var vignettePercentage = clampf((currentSeenTime - vignetteStartPercent) / (maxSeenTime - vignetteStartPercent), 0.0, 1.0);
 	
-	vignetteColor.a = vignettePercentage * vignetteMaxAlpha;
-	$vignette.modulate = vignetteColor;
+	$vignette.modulate.a = vignettePercentage * vignetteMaxAlpha;
 	
 	$center/squarecontainer/progressbar.value = detectionPercentage;
+	$center/squarecontainer/progressbar.modulate.a = detectionPercentage * ringMaxAlpha;
+	
+	$center/squarecontainer/progressbar/exclamation.modulate.a = clampf((detectionPercentage - 0.4) * 2, 0, 1)
+	
+	if(detectionPercentage >= 1.0):
+		detected.emit();
