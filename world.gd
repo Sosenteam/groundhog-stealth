89e3cd7d7415e9342725b1b076eadcfa41042410
Beckett Lynch <86271920+cheeseboi5000@users.noreply.github.com>
2026-02-24 21:24:08 -0800

    added start of clock/day-night system, added arrow key controls, talk to beckett if you don't understand

--- world.gd
+++ world.gd
@@ -1,4 +1,19 @@
 extends Node2D
 
+@onready var player_node = $Player
+const clock_scene = preload("res://clock.tscn")
+var active_clock: Node
 func _physics_process(delta: float) -> void:
-	pass
+	if player_node.velocity.length() > 0:
+		active_clock.start_counting()
+
+func _ready() -> void:
+	
+	#creating clock
+	active_clock = clock_scene.instantiate()
+	active_clock.setup(70.0)
+	add_child(active_clock) 
+	
+	
+
+	
