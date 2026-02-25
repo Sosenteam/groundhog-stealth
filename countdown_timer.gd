19d240f69c01b16b2df6320a08720cff13031a1e
ChangaImaraSA <35507028+changaimaraSA@users.noreply.github.com>
2026-02-25 14:37:29 -0800

    Added Level Container Node and CountdownTimer to the World
        Added Level Container Node and CountdownTimer to the World and wired it up so levels are loaded into the level container and each automatically gets a countdown on load.
    
        Countdown timer which counts down from 3 before the level starts
        Sends out a countdown_finished when the countdown ends.

---
+++ countdown_timer.gd
@@ -0,0 +1,37 @@
+extends CanvasLayer
+
+signal countdown_finished
+
+@onready var label = $Label
+@onready var timer = $Timer
+
+var count = 3 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	timer.wait_time = 1.0
+	timer.timeout.connect(_on_timer_timeout)
+	update_display()
+	
+func start_countdown():
+	show()
+	update_display()
+	timer.start()
+
+func update_display():
+	if count > 0:
+		label.text = str(count)
+	else:
+		label.text = "GO!"
+
+func _on_timer_timeout():
+	count -= 1
+	update_display()
+
+	if count < -1: 
+		emit_signal("countdown_finished")
+		queue_free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- world.gd
+++ world.gd
@@ -1,19 +1,39 @@
 extends Node2D
 
+@onready var level_container = $LevelContainer
+@onready var countdown = $CountdownTimer
+
+@export var debug = {
+	"show_countdown": true,
+}
+
+var current_level_node = null
+
 @onready var player_node = $Player
 const clock_scene = preload("res://clock.tscn")
 var active_clock: Node
+func _ready():
+	# countdown.countdown_finished.connected(_start_level_clock)
+	load_level("res://levels/template level.tscn")
+
+func load_level(level_path: String):
+	if current_level_node:
+		current_level_node.queue_free()
+
+	var level_resource = load(level_path)
+	current_level_node = level_resource.instantiate()
+
+	level_container.add_child(current_level_node)
+
+	countdown.start_countdown()
+
+
 func _physics_process(delta: float) -> void:
 	if player_node.velocity.length() > 0:
 		active_clock.start_counting()
 
 func _ready() -> void:
-	
 	#creating clock
 	active_clock = clock_scene.instantiate()
 	active_clock.setup(70.0)
-	add_child(active_clock) 
-	
-	
-
-	
+	add_child(active_clock)
