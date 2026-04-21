d0aeb8e83083257f816fcfa3e1a7016f47533da3
Alec Singer <sosenteam@gmail.com>
2026-04-21 09:02:57 -0700

    controller support

--- game_over_fadeout.gd
+++ game_over_fadeout.gd
@@ -60,6 +60,13 @@ func _on_restart_pressed() -> void:
 	get_tree().paused = false;
 	get_tree().reload_current_scene()
 
+func _input(event: InputEvent) -> void:
+	if(event.is_action_pressed("ui_accept")):
+		_on_restart_pressed()
+	if(event.is_action_pressed("ui_select")):
+		_on_main_menu_pressed()
+		
+
 func _on_main_menu_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://main_screen.tscn")
